65dd2f79be036dc7db2bcec8f76a9b59dc0f94a5
SniktaVanManne <50053176+SniktaVanManne@users.noreply.github.com>
2019-05-17 11:23:47 -0600

    Update Main.java
    Practicing pulling

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, my name is Margareth");
     System.out.println("Hi Margareth, My name is Alexandre");
-    System.out.println("Hi Margareth, my name is Ryan! :P");
+    System.out.println("Hi Margareth, my name is Ryan! :)");
   }
 }
