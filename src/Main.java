a6f5b901d83af4f670048f456591798294ed86c6
Ryan Nielson <nie04011@byui.edu>
2019-05-17 11:13:02 -0600

    Ryan's Commit to ser17

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, my name is Margareth");
-    System.out.println("Hi Margareth, My name is Alexandre);
+    System.out.println("Hi Margareth, My name is Alexandre");
+    System.out.println("Hi Margareth, my name is Ryan! :)");
   }
 }
