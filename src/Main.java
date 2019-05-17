2cb720ea1ba9c68e6d35bffda0930431778e41ab
Alexandre Paixao <aspaixao@buyi.edu>
2019-05-17 12:56:54 +0100

    My hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello, my name is Margareth");
+    System.out.println("Hi Margareth, My name is Alexandre);
   }
 }
